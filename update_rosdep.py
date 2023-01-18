206e2cf4725928fee76c0d0a57c3f9a2ccca7d1e
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-01-18 17:06:38 +0900

    Fix update rosdep
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- update_rosdep.py
+++ update_rosdep.py
@@ -5,18 +5,19 @@ import subprocess
 import yaml
 
 parser = argparse.ArgumentParser()
-parser.add_argument("ros-distro")
-parser.add_argument("base-dire")
-parser.add_argument("rosdep-file")
+parser.add_argument("ros_distro")
+parser.add_argument("pkg_source")
+parser.add_argument("rosdep_file")
 args = parser.parse_args()
-path = pathlib.Path(args.file)
+path = pathlib.Path(args.rosdep_file)
 
-command = ["colcon", "list", "-tn", "--base-paths", args.base]
+command = ["colcon", "list", "-tn", "--base-paths", args.pkg_source]
 process = subprocess.run(command, capture_output=True, text=True)
 
 packages = yaml.safe_load(path.read_text()) if path.exists() else dict()
 for ros_pkg in process.stdout.strip().split():
-    deb_pkg = "ros-{}-{}".format(args.distro, ros_pkg.replace("_", "-"))
+    deb_pkg = "ros-{}-{}".format(args.ros_distro, ros_pkg.replace("_", "-"))
     packages[ros_pkg] = {"ubuntu": [deb_pkg]}
 
+path.parent.mkdir(parents=True, exist_ok=True)
 path.write_text(yaml.safe_dump(packages))
