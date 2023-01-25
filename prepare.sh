bf75725b3f4ff75903dd833442d92eb70e799caa
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-01-25 20:09:20 +0900

    Minimize diff
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- prepare.sh
+++ prepare.sh
@@ -4,16 +4,17 @@
 set -ex
 
 echo "Install dependencies"
+
 sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/sbuild
 sudo apt-get update
 sudo apt-get install -y mmdebstrap distro-info debian-archive-keyring ccache curl sbuild
 
+echo "Setup build environment"
 echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $DEB_DISTRO main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 sudo apt-get update
 sudo apt-get install -y python3-bloom python3-vcstool python3-rosdep python3-colcon-common-extensions
 
-echo "Setup build environment"
 mkdir -p ~/.cache/sbuild
 mmdebstrap --variant=buildd --include=apt,ccache \
   --customize-hook='chroot "$1" update-ccache-symlinks' \
@@ -28,8 +29,8 @@ chmod -R a+rwX ~/.cache/ccache
 cat << "EOF" > ~/.sbuildrc
 $build_environment = { 'CCACHE_DIR' => '/build/ccache' };
 $path = '/usr/lib/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games';
-$build_path = '/build/package/';
-$dsc_dir = 'package';
+$build_path = "/build/package/";
+$dsc_dir = "package";
 $unshare_bind_mounts = [ { directory => '/home/runner/.cache/ccache', mountpoint => '/build/ccache' } ];
 EOF
 echo "$SBUILD_CONF" >> ~/.sbuildrc
@@ -37,11 +38,18 @@ echo "\$extra_packages = ['$GITHUB_WORKSPACE/dists/$DEB_DISTRO/universe/binary-a
 
 cat ~/.sbuildrc
 
-# Only local file is supported unlike the original code.
-echo "Import source repository"
+echo "Checkout workspace"
+
 mkdir src
-vcs import src < "$REPOS_FILE"
-python3 $GITHUB_ACTION_PATH/scripts/apply_repos_config.py "$REPOS_CONF"
+case $REPOS_FILE in
+  http*)
+    curl -sSL "$REPOS_FILE" | vcs import src
+    ;;
+  *)
+    vcs import src < "$REPOS_FILE"
+    ;;
+esac
+python3 $GITHUB_ACTION_PATH/apply_repos_config.py "$REPOS_CONF"
 
 # Switch to the target branch to use released packages.
 echo "Switch to target branch"
